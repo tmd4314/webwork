30d9791857718749a3e3437113edc0f293815e83
tmd4314 <tmd4314@naver.com>
2025-04-28 18:20:09 +0900

    todo 완성

--- vue/3component/src/components/TodoItem.vue
+++ vue/3component/src/components/TodoItem.vue
@@ -1,4 +1,45 @@
 <template>
-  
+  <form @submit.prevent="onAddTodo">
+    <input v-model="inputTodo" required placeholder="new todo">
+    <button>할 일 추가</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="$emit('remove-todo', todo)">X</button>
+    </li>
+  </ul>
 </template>
-<script></script>
+<script>
+export default{
+  props:{
+    todos:{
+      type: Array,
+      required: true
+    },
+    newTodo:{
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      inputTodo: this.newTodo
+    }
+  },
+  watch:{
+    newTodo(newVal){
+      this.inputTodo = newVal;
+    }
+  },
+  methods: {
+    onAddTodo() {
+      if(this.inputTodo.trim()){
+        this.$emit('add-todo', this.inputTodo);
+        this.inputTodo = '';
+      }
+    }
+  }
+}
+  
+</script>
--- vue/3component/src/views/TodoView.vue
+++ vue/3component/src/views/TodoView.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
-    <todo-item/>
+    <todo-item
+      :todos = "todos"
+      :new-todo = "newTodo"
+      @add-todo = "addTodo"
+      @remove-todo = "removeTodo"
+    />
   </div>
 </template>
 <script>
   import TodoItem from '@/components/TodoItem.vue';
+  let id = 1; // 여기에 id를 먼저 선언해야 해
   export default {
-    components : {TodoItem}
+    components : {TodoItem},
+    data() {
+      return {
+        newTodo: '',
+        todos: [
+          { id: id++, text: 'HTML 배우기' },
+          { id: id++, text: 'JavaScript 배우기' },
+          { id: id++, text: 'Vue 배우기' }
+        ]
+      }
+    },
+    methods: {
+      addTodo(newTodoText) {
+        this.todos.push({id: id++, text: newTodoText});
+        this.newTodo = '';
+      },
+      removeTodo(todo) {
+        this.todos = this.todos.filter(t => t.id !== todo.id);
+      }
+    }
   }
 </script>
